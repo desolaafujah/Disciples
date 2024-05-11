7c47f01986c75f87d8e24e47308f96f0bc8a3dc2
dfujah <dfujah@umich.edu>
2024-05-11 17:29:18 -0400

    Figured out adding user data to the Disciples database

--- routes.py
+++ routes.py
@@ -3,28 +3,32 @@
 #user authentication to save user information into database 
 
 
-from forms import registerCheck, registrationForm
+from forms import registrationForm
 from flask import Flask, render_template, redirect, request, jsonify, flash
 from flask_pymongo import PyMongo 
 import pymongo
 from bson import ObjectId
 from pymongo import MongoClient
 import bcrypt
-import datetime 
+import datetime
+from flask import request 
 
 # import smtplib -> used for sending emails
+app = Flask(__name__)
+# add a secret key
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Disciples'
 
 # db name is Disciples
 client = MongoClient("mongodb://localhost:27017/Disciples")
-
-
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/Disciples'
+db = client["Disciples"]
+usersCollections = db["users"] #a table
 
 # initializes PyMongo to Flask application
 mongo = PyMongo(app)
-db = client["Disciples"]
+
+
+# Disciples is the database name 
+# users is the table name in the Disciples database
 
 
 
@@ -32,36 +36,43 @@ db = client["Disciples"]
 @app.route('/register', methods=['POST'])
 #function to register user and log information into the database
 def register(username, password):
-    form = registrationForm()
-    if form.registerCheck(username):
+    form = registrationForm(request.form)
+
+    # check if the username already exists
+    if form.validate_on_submit():
+        if not form.registerCheck(form.username.data):
+            return jsonify({'message': 'Username already exists'}), 400
+
+    
 
         # add user info to the database
-        usersCollections = db["users"] #a table
-        # Disciples is the database name 
-        # users is the table name in the Disciples database
-        discipleData = usersCollections(
-            firstName = form.firstName,
-            lastName = form.lastName,
-            userName = form.username,
-            email = form.email,
-            churchAffiliation = form.church,
-            spiritualBirthday = form.spirthday,
-            password = form.password
-        )
-        
+        discipleData = {
+            "firstName": form.firstName.data,
+            "lastName": form.lastName.data,
+            "userName" : form.username.data,
+            "email" : form.email.data,
+            "churchAffiliation" :form.church.data,
+            "spiritualBirthday": form.spirthday.data.strftime('%Y-%m-%d'),
+            "password": form.password.data
+        }
+            
         # how do i insert this data into the database?
-        
+        usersCollections.insert_one(discipleData)
 
+        flash('Registered successfully!', 'success')
+        # return redirect(url_for('home')) -> this would redirect to the home page when there is one
 
-    else:
-        flash('Username taken!')
-        #could also do the flash message thing
-
+    
+    flash('Invalid form data', 'danger')
+    # return redirect(url_for('home'))
+    
 # for login page, use GET for rendering pages
 # POST is used to submit data to the server (Flask)
 
 
-
+@app.route('/')
+def home():
+    return render_template('home.html') #doesn't exist yet fr
 #DATABASE
 
 # Disciples is the database name 
